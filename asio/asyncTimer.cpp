da2a68b7233ef67505ec0a297f19b804c4632b0a
Manny <egalli64@gmail.com>
2012-04-25 01:42:10 +0200

    http://thisthread.blogspot.com/2011/02/simple-deadline-timer.html

---
+++ asio/asyncTimer.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/thread/thread.hpp>
+
+class MyJob : public boost::noncopyable // 1
+{
+private:
+    boost::mutex mx_;
+    bool outOfTime_; // 2
+public:
+    MyJob() : outOfTime_(false) {}
+
+    void log(const char* message) // 3
+    {
+        boost::lock_guard<boost::mutex> lock(mx_);
+        std::cout << message << std::endl;
+    }
+
+    void timeout() // 4
+    {
+        outOfTime_ = true;
+        log("Timeout!");
+    }
+
+    void operator()() // 5
+    {
+        for(int i = 0; !outOfTime_; ++i)
+        {
+            std::ostringstream os;
+            os << '[' << i << ']';
+
+            log(os.str().c_str());
+            boost::this_thread::sleep(boost::posix_time::seconds(1));
+        }
+    }
+};
+
+class MyWaitHandler
+{
+private:
+    MyJob& job_; // 1
+
+public:
+    MyWaitHandler(MyJob& job) : job_(job) {}
+
+    void operator()(const boost::system::error_code&)
+    {
+        job_.timeout(); // 2
+    }
+};
+
+void tt01()
+{
+    boost::asio::io_service io; // 1
+    boost::asio::deadline_timer timer(io, boost::posix_time::seconds(3)); // 2
+
+    MyJob job; // 3
+    timer.async_wait(MyWaitHandler(job)); // 4
+
+    boost::thread thread(std::ref(job)); // 5
+    io.run();
+    job.log("I/O service completed");
+
+    thread.join();
+    job.log("Local thread completed");
+}
