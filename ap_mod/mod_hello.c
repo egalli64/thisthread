d414d8bb7d858e521379a5477173ec1834095429
Manny <egalli64@gmail.com>
2012-09-06 22:07:10 +0200

    simplest apache module

---
+++ ap_mod/mod_hello.c
@@ -0,0 +1,24 @@
+/**
+ * mod_hello.c
+ */
+
+#include <httpd.h>
+#include <http_config.h>
+
+static int handler(request_rec* r)
+{
+	ap_set_content_type(r, "text/plain");
+	ap_rputs("Hello Apache httpd module", r);
+	return OK;
+}
+
+static void hooks(apr_pool_t* p) 
+{
+	ap_hook_handler(handler, NULL, NULL, APR_HOOK_REALLY_FIRST);
+}
+
+module AP_MODULE_DECLARE_DATA hello_module = 
+{ 
+	STANDARD20_MODULE_STUFF, NULL, NULL, NULL, NULL, NULL, hooks
+};
+
