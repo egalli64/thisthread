3c090ddd68018b8b962a2e82f8900b5795bb1359
Manny <egalli64@gmail.com>
2018-03-12 14:31:29 +0100

    http://thisthread.blogspot.com/2018/03/boost-asio-strand-example.html

--- asio/strand.cpp
+++ asio/strand.cpp
@@ -1,68 +1,88 @@
 /**
- * Boost ASIO strand post example
- * For information and comments see: http://thisthread.blogspot.com/2012/04/post-on-asio-strand.html
+ * ASIO strand to synchronise
  *
- * Based on http://www.boost.org/doc/libs/1_49_0/doc/html/boost_asio/tutorial/tuttimer5/src.html
+ * Author:   Manny egalli64@gmail.com
+ * Info:     http://thisthread.blogspot.com/2018/03/boost-asio-strand-example.html
+ * Based on: Timer.5 - Synchronising handlers in multithreaded programs
+ *           http://www.boost.org/doc/libs/1_66_0/doc/html/boost_asio/tutorial/tuttimer5.html
  */
-
 #include <iostream>
+#include <functional>
+#include <thread>
+#include <boost/asio/io_context_strand.hpp>
+#include "basic_skills.h"
 #include <boost/asio.hpp>
-#include <boost/thread.hpp>
-
-namespace ba = boost::asio;
 
 namespace
 {
-    class Printer
-    {
-    private:
-        ba::strand strand_;
-        int count_;
+	class Printer
+	{
+	private:
+		ba::io_context::strand strand_;
+		int count_;
 
-        void print(const char* msg)
-        {
-            std::cout << boost::this_thread::get_id() << ' ' << msg << ' ' << count_ << std::endl;
-        }
+		void print(const char* msg)
+		{
+			std::cout << '{' << std::this_thread::get_id() << "} " << msg << ": " << count_ << " " << std::flush;
+		}
 
-        void print1()
-        {
-            if(count_ > 0)
-            {
-                print("print one");
-                --count_;
-                strand_.post(std::bind(&Printer::print1, this));
-            }
-        }
+		void print1()
+		{
+			if (count_ > 0)
+			{
+				print("one");
+				--count_;
 
-        void print2()
-        {
-            if(count_ > 0)
-            {
-                print("print two");
-                --count_;
-                strand_.post(std::bind(&Printer::print2, this));
-            }
-        }
+				strand_.post(std::bind(&Printer::print1, this));
+			}
+		}
 
-    public:
-        Printer(ba::io_service& aios, int count) : strand_(aios), count_(count)
-        {
-            strand_.post(std::bind(&Printer::print1, this));
-            strand_.post(std::bind(&Printer::print2, this));
-        }
+		void print2()
+		{
+			if (count_ > 0)
+			{
+				print("two");
+				--count_;
+				strand_.post(std::bind(&Printer::print2, this));
+			}
+		}
 
-        ~Printer()
-        {
-            print("dtor");
-        }
-    };
+	public:
+		Printer(ba::io_context& io, int count) : strand_(io), count_(count)
+		{
+			strand_.post(std::bind(&Printer::print1, this));
+			strand_.post(std::bind(&Printer::print2, this));
+		}
+
+		~Printer()
+		{
+			print("dtor");
+			std::cout << std::endl;
+		}
+	};
 }
 
-void strand()
+void timer5(ba::io_context& io)
 {
-    ba::io_service aios;
-    Printer p(aios, 10);
-    boost::thread t(std::bind(&ba::io_service::run, &aios));
-    aios.run();
-    t.join();
+	std::cout << "5) Starting ... " << std::flush;
+
+	Printer printer(io, 10);
+
+	// boost bind approach
+	//std::thread thread(boost::bind(&ba::io_context::run, &io));
+
+	// std bind approach
+	// error C2672: 'std::bind': no matching overloaded function found
+	// error C2783: 'std::_Binder<std::_Unforced,_Fx,_Types...> std::bind(_Fx &&,_Types &&...)': could not deduce template argument for '_Fx'
+	//std::thread thread(std::bind(&ba::io_context::run, &io));
+
+	// std bind patched approach
+	//auto run = static_cast<ba::io_context::count_type(ba::io_service::*)()>(&ba::io_context::run);
+	//std::thread thread(std::bind(run, &io));
+
+	// lambda approach
+	std::thread thread([&io] { io.run(); });
+
+	io.run();
+	thread.join();
 }
