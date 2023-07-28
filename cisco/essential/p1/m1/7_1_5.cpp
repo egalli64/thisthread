d2927ad87e8d927ac9de5a43eb9ec8343e2e77f6
Emanuele Galli <egalli64@gmail.com>
2023-07-28 13:08:24 +0200

    setbase

---
+++ cisco/essential/p1/m1/7_1_5.cpp
@@ -0,0 +1,22 @@
+/*
+    Cisco Network Academy - CPA - Programming Essentials in C++
+    https://www.netacad.com/courses/programming/essentials-programming-c-plus-plus
+
+    My repository: https://github.com/egalli64/thisthread
+
+     1.7.1.4 Connecting with the real world: input and output
+
+     I/O manipulator by setbase
+ */
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    int value = 255;
+    std::cout << "A value: " << value << '\n';
+    std::cout << "Hex format (by setbase): " << std::setbase(16) << value << '\n';
+    std::cout << "Hex format is still valid: " << value << '\n';
+    std::cout << "Switch back to decimal: " << std::setbase(10) << value << '\n';
+    std::cout << "Oct format: " << std::setbase(8) << value << '\n';
+}
