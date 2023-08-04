e0bec301d943b0174591a5afd5f8a5e7aceebf4c
Emanuele Galli <egalli64@gmail.com>
2023-08-04 08:07:27 +0200

    more examples

--- cisco/essential/p2/m2/6_1_3.cpp
+++ cisco/essential/p2/m2/6_1_3.cpp
@@ -15,22 +15,43 @@ int main()
 {
     int data[]{1, 2, 3, 4, 5};
 
-    int *const cpi = data + 2;
-    std::cout << "The third element of data, " << data[2] << ", is pointed by cpi, " << *cpi << '\n';
-    *cpi += 10;
-    // cpi += 1;
-    std::cout << "cpi is a constant pointer to int, can't change, but its pointed can: " << *cpi << '\n';
+    {
+        int *const cpi = data + 2;
+        std::cout << "The third element of data, " << data[2] << ", is pointed by cpi, " << *cpi << '\n';
+        *cpi += 10;
+        // cpi += 1;
+        std::cout << "cpi is a constant pointer to int, can't change, but its pointed can: " << *cpi << '\n';
+    }
 
-    const int * pic = data + 2;
-    std::cout << "The third element of data, " << data[2] << ", is pointed by pic, " << *pic << '\n';
-    // *pic += 10;
-    pic += 1;
-    std::cout << "pic is a pointer to constant int, it can change, but its pointed can't: " << *pic << '\n';
+    {
+        const int *pic = data + 2;
+        std::cout << "The third element of data, " << data[2] << ", is pointed by pic, " << *pic << '\n';
+        // *pic += 10;
+        pic += 1;
+        std::cout << "pic is a pointer to constant int, it can change, but its pointed can't: " << *pic << '\n';
+    }
 
+    {
+        int const *pci = data + 2;
+        std::cout << "The third element of data, " << data[2] << ", is pointed by pic, " << *pci << '\n';
+        // *pci += 10;
+        pci += 1;
+        std::cout << "pci is a pointer to constant int, it can change, but its pointed can't: " << *pci << '\n';
+    }
+
+    {
+        const int *const cpic = data + 2;
+        std::cout << "The third element of data, " << data[2] << ", is pointed by cpic, " << *cpic << '\n';
+        // *cpic += 10;
+        // cpic += 1;
+        std::cout << "cpic is a constant pointer to constant int, it can't change, as its pointed: " << *cpic << '\n';
+    }
     
-    const int * const cpic = data + 2;
-    std::cout << "The third element of data, " << data[2] << ", is pointed by cpic, " << *cpic << '\n';
-    // *cpic += 10;
-    // cpic += 1;
-    std::cout << "cpic is a constant pointer to constant int, it can't change, as its pointed: " << *cpic << '\n';
+    {
+        int const *const cpci = data + 2;
+        std::cout << "The third element of data, " << data[2] << ", is pointed by cpic, " << *cpci << '\n';
+        // *cpci += 10;
+        // cpci += 1;
+        std::cout << "cpci is a constant pointer to constant int, it can't change, as its pointed: " << *cpci << '\n';
+    }
 }
